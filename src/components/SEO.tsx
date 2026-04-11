cd93dc3a3052d30a814982b57145f272edf8ef96
complaintdone <chanter-finds.7b@icloud.com>
2026-04-11 20:28:55 +0100

    Remove duplicate Search Console meta tag (already in index.html)

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -25,8 +25,6 @@ export const SEO = ({
 
   return (
     <Helmet>
-      {/* Google Search Console verification */}
-      <meta name="google-site-verification" content="6K4fH3AHvJ79HNtEcGUqwAsUUdwi_-bJSnhAlCc6CuY" />
       {/* Primary Meta Tags */}
       <title>{fullTitle}</title>
       <meta name="title" content={fullTitle} />
